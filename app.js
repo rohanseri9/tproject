3bdb1fa7329b8110cd1502993dbf18d153bab223
Seri Manju <rohanseri9@gmail.com>
2022-11-09 12:12:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function myfuction(){
+    location.replace("http://127.0.0.1:5501/mer/dashboard.html")
+ 
+    
+} 
+
---
+++ app1.js
@@ -0,0 +1,4 @@
+function myfuction(){
+    location.replace("http://127.0.0.1:5501/mer/main.html")
+    alert("COMFORM TO LOGOUT")
+}
